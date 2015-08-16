8719d7aee6930e45f48a034edcc8f86652826596
Leon Bentrup <leon@leonadi.de>
2015-08-16 15:56:52 -0400

    Fix prefix change check

--- functions/schedule.js
+++ functions/schedule.js
@@ -11,27 +11,31 @@ var job = null;
 var lastError = null;
 var lastPrefix = 'Not yet';
 
+function jobFunc(force, cb) {
+  lastRun = new Date();
+  numRuns += 1;
+  var newPrefix = ip.prefix()[0];
+  if (force || newPrefix !== lastPrefix) {
+    return cf.updateCloudflare(function (err) {
+      lastChangeRun = lastRun;
+      numChangeRuns += 1;
+      lastPrefix = newPrefix;
+      if (err) {
+        lastError = err;
+        if (cb) return cb(err);
+        return console.error(err);
+      }
+      if (cb) cb(null, true);
+    });
+  }
+  if (cb) cb(null, false)
+}
+
 function scheduleJob(cb) {
   Config.findOne({}, function (err, config) {
     if(err) return cb(err);
     if(!config) return cb('Not yet configured');
-    job = schedule.scheduleJob(config.interval, function () {
-      lastRun = Date.now();
-      numRuns += 1;
-      var newPrefix = ip.prefix();
-      if (newPrefix !== lastPrefix) {
-        cf.updateCloudflare(function (err) {
-          lastChangeRun = lastRun;
-          numChangeRuns += 1;
-          lastPrefix = newPrefix;
-          if (err) {
-            lastError = err;
-            return console.error(err);
-          }
-          
-        });
-      }
-    });
+    job = schedule.scheduleJob(config.interval, jobFunc);
   });
 }
 
@@ -46,5 +50,6 @@ function status() {
   };
 }
 
+exports.jobFunc = jobFunc;
 exports.schedule = scheduleJob;
 exports.status = status;
--- routes/main.js
+++ routes/main.js
@@ -1,15 +1,22 @@
-var cf = require('../functions/cloudflare');
 var schedule = require('../functions/schedule');
 
 module.exports = function (router) {
   router.get('/', function (req, res) {
-    
     res.render('index', {errors: req.flash('errors'), status: schedule.status()});
   });
 
   router.post('/update', function (req, res, next) {
     req.redirect = '/';
-    cf.updateCloudflare(function (err, result) {
+    schedule.jobFunc(false, function (err) {
+      if(err) return next(err);
+      req.flash('errors', 'Update successful');
+      res.redirect('/');
+    });
+  });
+  
+  router.post('/forceupdate', function (req, res, next) {
+    req.redirect = '/';
+    schedule.jobFunc(true, function (err) {
       if(err) return next(err);
       req.flash('errors', 'Update successful');
       res.redirect('/');
